3b065c552f810239bbd60f2519f36e9f54bf8953
Omni Devel <safephone1.12bratchikov@yandex.ru>
2025-06-12 22:16:32 +0300

    feat: Enhance config handling and user validation in the application

--- src/data/structs/mod.rs
+++ src/data/structs/mod.rs
@@ -8,6 +8,18 @@ pub struct User {
     pub password: String,
 }
 
+impl User {
+    pub fn verify(self) -> Result<(), String> {
+        if self.username.contains(':') {
+            Err(String::from("Username cannot contains a `:` symbol!"))
+        } else if self.password.contains(':') {
+            Err(String::from("Password cannot contains a `:` symbol!"))
+        } else {
+            Ok(())
+        }
+    }
+}
+
 #[derive(Deserialize)]
 pub struct Config {
     users: Vec<User>,
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,37 @@ pub mod handlers;
 pub mod database;
 pub mod data;
 
+use std::process::ExitCode;
+
+use tokio::fs;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> ExitCode {
+    let config_file_is_exists = tokio::fs::try_exists("config.yml").await;
+
+    if config_file_is_exists.is_err() || !config_file_is_exists.unwrap() {
+        eprintln!("Please, create and fill a config file `config.yml`. Example config: `config.yml.example`!");
+
+        return ExitCode::from(1);
+    }
+
+    let config = match fs::read_to_string("config.yml").await {
+        Ok(c) => {
+            match serde_yaml::from_str::<data::structs::Config>(&c) {
+                Ok(config) => config,
+                Err(_) => {
+                    eprintln!("Incorrect `config.yml` config file!");
+
+                    return ExitCode::from(1);
+                }
+            }
+        },
+        Err(_) => {
+            eprintln!("Incorrect `config.yml` config file!");
+
+            return ExitCode::from(1);
+        }
+    };
+
+    ExitCode::from(0)
 }
